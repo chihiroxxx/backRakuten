5f13f3a321dfe432ffddfab3a0200bbb8229e1df
chihiroxxx <maturi1000@gmail.com>
2021-08-26 15:58:58 +0900

    create seedコメントアウト CMD修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,11 @@ set -e
 rm -f /app/tmp/pids/server.pid
 # WARNING:createとseedはfargateの初回のみ実行
 # WARNING:タスクを個々に作って実行の方がいいかも
-bundle exec rails db:create
+
+# 下記のcreare,seedはいつもと同じく初回のみ
+# bundle exec rails db:create
 bundle exec rails db:migrate
-bundle exec rails db:seed
+# bundle exec rails db:seed
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
