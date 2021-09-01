9bd1264b32ec0130df7386f412e1d2736e154934
chihiroxxx <maturi1000@gmail.com>
2021-09-01 10:51:19 +0900

    実験の結果、valueFromでvalueとってこれてないような感じだなあ...

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,9 @@ rm -f /app/tmp/pids/server.pid
 # WARNING:タスクを個々に作って実行の方がいいかも
 
 # 下記のcreare,seedはいつもと同じく初回のみ
-bundle exec rails db:create
+# bundle exec rails db:create
 bundle exec rails db:migrate
-bundle exec rails db:seed
+# bundle exec rails db:seed
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
