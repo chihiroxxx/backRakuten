28aee89fbf820bb2f1af24e80cbf7a31c73ef8ae
chihiroxxx <maturi1000@gmail.com>
2021-09-01 17:46:30 +0900

    ALB一本化

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'mypage', to: 'users#me'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-  get 'csv', to: 'books#get_csv'
+  namespace 'api' do
+    namespace 'v1' do
+      get 'mypage', to: 'users#me'
+      post 'login', to: 'sessions#create'
+      delete 'logout', to: 'sessions#destroy'
+      get 'csv', to: 'books#get_csv'
 
-  root 'home#index'
-  resources :books, only: [:index,:create, :update, :destroy]
-  resources :users, only: [:new,:create]
+      root 'home#index'
+      resources :books, only: [:index,:create, :update, :destroy]
+      resources :users, only: [:new,:create]
+    end
+  end
 end
