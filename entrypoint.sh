460632c0798aaf3b71083d3bc98d4d1cfc4acabd
chihiroxxx <maturi1000@gmail.com>
2021-09-03 12:55:12 +0900

    dbのコマンド設定！！修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,14 @@ rm -f /app/tmp/pids/unicorn.pid
 # WARNING:タスクを個々に作って実行の方がいいかも
 
 # 下記のcreare,seedはいつもと同じく初回のみ
-bundle exec rails db:create
-bundle exec rails db:migrate
-bundle exec rails db:seed
+# develop
+# bundle exec rails db:create
+# bundle exec rails db:migrate
+# bundle exec rails db:seed
+
+bundle exec rails db:create RAILS_ENV=production
+bundle exec rails db:migrate RAILS_ENV=production
+bundle exec rails db:seed RAILS_ENV=production
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
